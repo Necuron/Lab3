5af5de5287998b3ee5056ac944e5328cf0f0bcce
vincentvu <vincentvu@5010-011.ODENG2.CARLETON.CA>
2015-09-17 11:52:14 -0400

    Added some method calls in AddressBook.java to test GIT commit.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,5 +17,11 @@ public class AddressBook {
 	public static void main(String[] args)
 	{
 		System.out.println("Address Book");
+		
+		AddressBook book = new AddressBook();
+		
+		BuddyInfo friend = new BuddyInfo();
+		book.addBuddy(friend);
+		book.removeBuddy(friend);
 	}
 }
