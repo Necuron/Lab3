b90a8c0e094abb20b786628a4da1b0559170adfe
vincentvu <vincentvu@5010-022.ODENG2.CARLETON.CA>
2015-09-24 11:03:49 -0400

    Committed after branching

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,7 +20,7 @@ public class AddressBook {
 	public static void main(String[] args)
 	{
 		System.out.println("Address Book");
-		
+		System.out.println("This is added after Branching");
 		AddressBook book = new AddressBook();
 		
 		BuddyInfo friend = new BuddyInfo();
